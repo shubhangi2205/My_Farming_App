0a1d01e90607b810f785dc2bca06a068a9278335
divyanshi <khannadivyanshi38@gmail.com>
2023-06-29 20:23:42 +0530

    unversion

---
+++ app/src/main/java/com/example/farmingapp/FarmingOutput.java
@@ -0,0 +1,135 @@
+package com.example.farmingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.farmingapp.models.CropModel;
+import com.example.farmingapp.models.FarmingOutputModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+
+public class FarmingOutput extends AppCompatActivity {
+    FirebaseFirestore db;
+    final String TAG = "firestore";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+
+
+        FarmingOutputModel farmingOutputModel = new FarmingOutputModel(
+                "Sugarcane",
+                2019,
+                "Winter",
+                200,
+                "Delhi"
+        );
+        saveFarmingOutputDetails(farmingOutputModel);
+        getFarmingOutputDetails(farmingOutputModel);
+    }
+
+    void saveFarmingOutputDetails(FarmingOutputModel farmingOutputModel){
+        Map<String, Object> farmingOutputMap = farmingOutputModel.mapAllTheData();
+        db.collection("farmer")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String farmerDocId = null;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                                String phoneNum = (String) document.get("Phone Number");
+                                if(Objects.equals(phoneNum, "123568898")) {
+                                    farmerDocId = document.getId();
+                                    break;
+                                }
+                            }
+                            db.collection("farmer")
+                                    .document(farmerDocId)
+                                    .collection("farming_output")
+                                    .add(farmingOutputMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            Log.d(TAG, "Data Saved at farming_output successfully");
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull @NotNull Exception e) {
+
+                                            Log.d(TAG, "Failed to Save data at crop_details");
+
+
+                                        }
+                                    });
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    void getFarmingOutputDetails(FarmingOutputModel farmingOutputModel){
+        Map<String, Object> farmingOutputDetailMap = farmingOutputModel.mapAllTheData();
+        db.collection("farmer")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String farmerDocId = null;
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                                String phoneNum = (String) document.get("Phone Number");
+                                if (Objects.equals(phoneNum, "123568898")) {
+                                    farmerDocId = document.getId();
+                                    break;
+                                }
+                            }
+                            //db.collection("crop_details")
+                            db.collection("farmer")
+                                    .document(farmerDocId)
+                                    .collection("farming_output")
+                                    .get()
+                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            ArrayList<String> cropNameList = new ArrayList<>();
+                                            if (task.isSuccessful()) {
+                                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                                    Log.d(TAG, document.getId() + " => " + document.getData());
+
+
+                                                }
+
+                                            } else {
+                                                Log.d(TAG, "Error getting documents: ", task.getException());
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+
+                });
+    };
+
+    void getFarmingOutputDetails(){
+
+    }
+}
---
+++ app/src/main/java/com/example/farmingapp/HomePage.java
@@ -0,0 +1,74 @@
+package com.example.farmingapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity {
+
+    TextView t1;
+    TextView t2;
+    TextView t3;
+    TextView t4;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+
+        t1 = findViewById(R.id.expense);
+        t2 = findViewById(R.id.harvest);
+        t3 = findViewById(R.id.dashboard);
+        t4 = findViewById(R.id.suggestion);
+
+        t1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HomePage.this,CropDetails.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+
+        t2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HomePage.this,CropDetails.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+
+        t3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HomePage.this,CropDetails.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+        t4.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(HomePage.this,CropDetails.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+    }
+}
